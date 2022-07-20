673babd20e8c2be7fcb833d8f0015d642f20e88e
Batuhan ARIK <batuhanarik123@gmail.com>
2022-07-20 15:19:08 +0300

    first commit

---
+++ lib/model/burc.dart
@@ -0,0 +1,27 @@
+class Burc{
+  final String _burcAdi;
+  final String _burcTarihi;
+  final String _burcDetayi;
+  final String _burcKucukResim;
+  final String _burcBuyukResim;
+
+  Burc(this._burcAdi, this._burcTarihi, this._burcDetayi, this._burcKucukResim, this._burcBuyukResim);
+   
+  get burcAdi => _burcAdi;
+
+  get burcTarihi => _burcTarihi;
+
+  get burcDetayi => _burcDetayi;
+
+  get burcKucukResim => _burcKucukResim;
+
+  get burcBuyukResim => _burcBuyukResim;
+
+  
+
+ @override
+ String toString() {
+   return '$_burcAdi - $_burcBuyukResim \n';
+ }
+
+}
